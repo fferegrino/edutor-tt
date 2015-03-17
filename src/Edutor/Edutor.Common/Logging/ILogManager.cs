32cbfce24aba55bb0017fc02e49243ef6e995131
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-03-16 19:56:09 -0600

    Container bindings

---
+++ src/Edutor/Edutor.Common/Logging/ILogManager.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edutor.Common.Logging
+{
+    public interface ILogManager
+    {
+        ILog GetLog(Type typeAssociatedWithRequestedLog);
+    }
+
+    public class LogManagerAdapter : ILogManager
+    {
+
+        public ILog GetLog(Type typeAssociatedWithRequestedLog)
+        {
+            return LogManager.GetLogger(typeAssociatedWithRequestedLog);
+        }
+    }
+}
---
+++ src/Edutor/Edutor.Web.Api/App_Start/NinjectConfigurator.cs
@@ -0,0 +1,33 @@
+﻿using Edutor.Common;
+using Edutor.Common.Logging;
+using log4net.Config;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edutor.Web.Api
+{
+    public class NinjectConfigurator
+    {
+        public void Configure(IKernel container)
+        {
+            AddBindings(container);
+        }
+
+        private void AddBindings(IKernel container)
+        {
+            ConfigureLog4Net(container);
+            container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
+        }
+
+        private void ConfigureLog4Net(IKernel container)
+        {
+            XmlConfigurator.Configure();
+            var logManager = new LogManagerAdapter();
+            container.Bind<ILogManager>().ToConstant(logManager);
+        }
+    }
+}
