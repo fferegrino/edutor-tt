269fbf092613b719318156902c918573e87aa194
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-03-16 21:56:01 -0600

    Start of database configuration

---
+++ src/Edutor/Edutor.Data.SqlServer/Mapping/VersionedClassMap.cs
@@ -0,0 +1,17 @@
+﻿using Edutor.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edutor.Data.SqlServer.Mapping
+{
+    public abstract class VersionedClassMap<T> : FluentNHibernate.Mapping.ClassMap<T> where T : IVersionedEntity
+    {
+        protected VersionedClassMap()
+        {
+            // TODO: Page 78
+        }
+    }
+}
--- src/Edutor/Edutor.Data/Entities/User.cs
+++ src/Edutor/Edutor.Data/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Edutor.Data.Entities
 {
-    public class User
+    public class User : IVersionedEntity
     {
         public virtual int UserId { get; set; }
 
