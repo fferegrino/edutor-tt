5f9e1d53b544ef950f3778b49a03df1a6d07fd93
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-04-24 00:20:15 -0500

    Controllers

---
+++ src/Edutor/Edutor.Web.Api/Controllers/StudentsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Edutor.Web.Api.Controllers
+{
+    [Edutor.Web.Common.UnitOfWorkActionFilter]
+    public class StudentsController : ApiController
+    {
+    }
+}
