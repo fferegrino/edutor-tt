18987442c03ae5be9e14f931574d230cdd74ecaf
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-05-11 13:49:16 -0500

    Fixing datasize

--- src/Edutor/Edutor.Db/Tables/Groups.sql
+++ src/Edutor/Edutor.Db/Tables/Groups.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Groups]
 (
 	[GroupId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(10) NOT NULL,
-	[Subject] VARCHAR(20) NOT NULL,
+    [Name] VARCHAR(20) NOT NULL,
+	[Subject] VARCHAR(35) NOT NULL,
 	[FromDate] DATE NOT NULL,
 	[ToDate] DATE  NULL
 )
