3cea3f0d58eb53401801fb4ef9ea21f4732ff415
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-05-13 10:05:17 -0500

    Questions mapping p II

--- src/Edutor/Edutor.Data/Entities/PossibleAnswer.cs
+++ src/Edutor/Edutor.Data/Entities/PossibleAnswer.cs
@@ -8,6 +8,37 @@ namespace Edutor.Data.Entities
 {
     public class PossibleAnswer : IVersionedEntity
     {
+        public virtual int QuestionId { get; set; }
+        public virtual int PossibleAnswerId { get; set; }
+        public virtual string Text { get; set; }
+
+        public virtual Question Question { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            PossibleAnswer p = obj as PossibleAnswer;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (QuestionId == p.QuestionId) && (PossibleAnswerId == p.PossibleAnswerId);
+        }
+
+        public override int GetHashCode()
+        {
+            return QuestionId ^ PossibleAnswerId;
+        }
+
         public virtual byte[] Version { get; set; }
     }
 }
