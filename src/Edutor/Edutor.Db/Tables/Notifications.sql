4fc3e8b76e248be3ac21aa06141331473544bb79
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-05-13 13:12:39 -0500

    Sorry M8

--- src/Edutor/Edutor.Db/Tables/Notifications.sql
+++ src/Edutor/Edutor.Db/Tables/Notifications.sql
@@ -2,6 +2,8 @@
 (
 	[NotificationId] INT NOT NULL PRIMARY KEY IDENTITY,
 	[SchoolUserId] INT NOT NULL,
+	[GroupId] INT NOT NULL,
 	[Text] VARCHAR(200) NOT NULL,
-	CONSTRAINT [FK_Notifications_ToSchoolUsers] FOREIGN KEY ([SchoolUserId]) REFERENCES [Users]([UserId])
+	CONSTRAINT [FK_Notifications_ToSchoolUsers] FOREIGN KEY ([SchoolUserId]) REFERENCES [Users]([UserId]),
+	CONSTRAINT [FK_Notifications_ToGroups] FOREIGN KEY ([GroupId]) REFERENCES [Groups]([GroupId])
 )
