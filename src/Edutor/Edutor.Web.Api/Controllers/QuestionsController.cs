4bc1eb69916ca1391bc6a75c17f914b0cd039a55
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-05-31 17:46:13 -0500

    Valdiation actionfilter

--- src/Edutor/Edutor.Web.Api/Controllers/QuestionsController.cs
+++ src/Edutor/Edutor.Web.Api/Controllers/QuestionsController.cs
@@ -6,6 +6,7 @@ using Edutor.Web.Api.Models.NewModels;
 using Edutor.Web.Api.Models.ReturnTypes;
 using Edutor.Web.Api.UpdateProcessing;
 using Edutor.Web.Common;
+using Edutor.Web.Common.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -96,6 +97,7 @@ namespace Edutor.Web.Api.Controllers
         [HttpPost]
         [ResponseType(typeof(Question))]
         [Authorize(Roles = Constants.RoleNames.SchoolUser)]
+        [ValidationActionFilter]
         public IHttpActionResult AddQuestion(NewQuestion question)
         {
             var ret = _postQuestion.AddQuestion(question);
