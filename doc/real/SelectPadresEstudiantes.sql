9ab870b9a8e1118a1c57a5e8e0218bcd94758223
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-06-14 23:10:45 -0500

    Real data doc

--- doc/real/SelectPadresEstudiantes.sql
+++
@@ -1,6 +0,0 @@
-SELECT 'curp padre|curp hijo|nombre hijo|relacion|direccion|telefono'
-UNION
-SELECT T.Curp + '|' + S.Curp + '|' +  S.Name + '|' + S.TutorRelationship + '|' + T.[Address] + '|' + T.Phone
-FROM Users T
-	INNER JOIN Students S
-	ON T.UserId = S.TutorId
--- doc/real/deleteandreseeed.sql
+++
@@ -1,9 +0,0 @@
-delete from Groups where GroupId > 0
-
-dbcc checkident('Groups',RESEED,0)
-
-delete Students
-dbcc checkident('Students',RESEED,0)
-
-delete Users where UserId > 1
-dbcc checkident('Users',RESEED,1)
